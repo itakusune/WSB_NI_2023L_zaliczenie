b943ebf36b07eaea70d149000c153236964e8e67
itakusune <123702517+itakusune@users.noreply.github.com>
2023-06-25 12:25:58 +0200

    Wymagane
    ala ma kota

--- Jakub_22811/test.java
+++ Jakub_22811/test.java
@@ -2,8 +2,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        // Prints "Hello, World" in the terminal window.
+        // Prints "Zegnaj, Ziemio" in the terminal window.
         System.out.println("Hello, World");
     }
 
 }
+babababbaba
